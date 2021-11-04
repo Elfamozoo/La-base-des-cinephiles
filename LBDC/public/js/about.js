91752ef98bcb08a933f4d385c03f6141f2bcdd19
Elfamozoo <90515083+Elfamozoo@users.noreply.github.com>
2021-11-04 16:28:06 +0100

    Journée 04/11/21

--- LBDC/public/js/about.js
+++ LBDC/public/js/about.js
@@ -11,5 +11,27 @@ fetch(`${movie_detail_http}${movie_id}?` + new URLSearchParams({
 
 .then(res => res.json())
 .then(data => {
-    console.log(data)
+    setupMovieInfo(data);
 })
+
+const setupMovieInfo = (detail) => {
+    const movieName = document.querySelector('.film-nom');
+    const genre = document.querySelector('.genre');
+    const description = document.querySelector('.description');
+    const title = document.querySelector('title');
+    const backdrop = document.querySelector('.film-info');
+
+
+    title.innerHTML = movieName.innerHTML = detail.title;
+    genre.innerHTML = `${detail.release_date.split('-')[0]} | `
+    // Boucle pour le genre
+    for(let i = 0; i < detail.genre.length; i++){
+        genre.innerHTML += detail.genre[i].name + formatString(i, detail.genre.length);
+    }
+}
+
+// Fonction qui permets d'espacer les genres
+const formatString = (currentIndex, maxIndex) => {
+    // Si le genre actuel n'est pas le dernier alors cela retourne "," sinon si c'est le dernier cela ne fait rien.
+    return (currentIndex == maxIndex - 1) ? '' : ', ';
+}
--- LBDC/public/js/api.js
+++ LBDC/public/js/api.js
@@ -1,13 +1,16 @@
-let api_key = "a42944988ef08836553202a6f37bf004"
+const api_key = "a42944988ef08836553202a6f37bf004"
 
-let img_url = "https://image.tmdb.org/t/p/w500/"
+const img_url = "https://image.tmdb.org/t/p/w500/"
 
-let original_img_url = "https://image.tmdb.org/t/p/original/"
+const original_img_url = "https://image.tmdb.org/t/p/original"
 
-let base_uri = "https://api.themoviedb.org/3/"
+const base_uri = "https://api.themoviedb.org/3/"
 
-let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?api_key=a42944988ef08836553202a6f37bf004&language=fr-FR"
+const genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?api_key=a42944988ef08836553202a6f37bf004&language=fr-FR"
 
-let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?api_key=a42944988ef08836553202a6f37bf004&language=fr-FR"
+const movie_genres_http = "https://api.themoviedb.org/3/discover/movie?api_key=a42944988ef08836553202a6f37bf004&language=fr-FR"
 
-let movie_detail_http = "https://api.themoviedb.org/3/movie?api_key=a42944988ef08836553202a6f37bf004&language=fr-FR"
+const movie_detail_http = "https://api.themoviedb.org/3/movie"
+
+// const movie_detail_http = "https://api.themoviedb.org/3/movie?api_key=a42944988ef08836553202a6f37bf004&language=fr-FR"
+
