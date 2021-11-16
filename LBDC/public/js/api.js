7e13c2dba84714971a723af297ef447701d99db4
Elfamozoo <90515083+Elfamozoo@users.noreply.github.com>
2021-11-16 15:56:16 +0100

    Update api.js

--- LBDC/public/js/api.js
+++ LBDC/public/js/api.js
@@ -4,6 +4,8 @@ const img_url = "https://image.tmdb.org/t/p/w500/"
 
 const original_img_url = "https://image.tmdb.org/t/p/original"
 
+const translation_url = "https://api.themoviedb.org/3/configuration/languages?api_key=a42944988ef08836553202a6f37bf004"
+
 const base_uri = "https://api.themoviedb.org/3/"
 
 const genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?api_key=a42944988ef08836553202a6f37bf004&language=fr-FR"
