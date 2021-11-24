ebc6c1a119c5d400c6405179c0114fc720b0b80d
Elfamozoo <90515083+Elfamozoo@users.noreply.github.com>
2021-11-24 15:27:38 +0100

    test login

--- LBDC/server.js
+++ LBDC/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const path = require ('path')
+const path = require('path')
 const mysql = require('mysql');
 const session = require('express-session');
 const bodyParser = require('body-parser');
@@ -19,8 +19,8 @@ app.get('/:id', (req, res) => {
     res.sendFile(path.join(initial_path, "about.html"));
 })
 
-app.get('/', function(req, response) {
-	response.sendFile(path.join(__dirname + '/login.html'));
+app.get('/', function (req, response) {
+    response.sendFile(path.join(__dirname + '/login.html'));
 });
 
 // Renvoi une erreur 404 si la route est invalide.
@@ -29,58 +29,62 @@ app.use((req, res) => {
 })
 
 
-app.listen(3000, ()=> {
-    console.log('Ecoute le port 3000........');
-})
-
-
 
 var connection = mysql.createConnection({
-	host     : 'localhost',
-	user     : 'root',
-	password : '',
-	database : 'nodelogin'
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'nodelogin'
 });
 
 app.use(session({
-	secret: 'secret',
-	resave: true,
-	saveUninitialized: true
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
 }));
-app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(bodyParser.json());
 
-app.get('/', function(request, response) {
-	response.sendFile(path.join(__dirname + '/login.html'));
+app.get('/', function (request, response) {
+    response.sendFile(path.join(__dirname + '/login.html'));
 });
 
-app.post('/auth', function(request, response) {
-	var username = request.body.username;
-	var password = request.body.password;
-	if (username && password) {
-		connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
-			if (results.length > 0) {
-				request.session.loggedin = true;
-				request.session.username = username;
-				response.redirect('/home');
-			} else {
-				response.send('Incorrect Username and/or Password!');
-			}			
-			response.end();
-		});
-	} else {
-		response.send('Please enter Username and Password!');
-		response.end();
-	}
+app.post('/auth', function (request, response) {
+    var username = request.body.username;
+    var password = request.body.password;
+    if (username && password) {
+        connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function (error, results, fields) {
+            if (results.length > 0) {
+                request.session.loggedin = true;
+                request.session.username = username;
+                response.redirect('/home');
+            } else {
+                response.send('Incorrect Username and/or Password!');
+            }
+            response.end();
+        });
+    } else {
+        response.send('Please enter Username and Password!');
+        response.end();
+    }
 });
 
-app.get('/home', function(request, response) {
-	if (request.session.loggedin) {
-		response.send('Welcome back, ' + request.session.username + '!');
-	} else {
-		response.send('Please login to view this page!');
-	}
-	response.end();
+app.get('/home', function (request, response) {
+    if (request.session.loggedin) {
+        response.send('Welcome back, ' + request.session.username + '!');
+    } else {
+        response.send('Please login to view this page!');
+    }
+    response.end();
 });
 
-app.listen(3001);
+
+
+
+
+
+app.listen(3000, () => {
+    console.log('Ecoute le port 3000........');
+})
